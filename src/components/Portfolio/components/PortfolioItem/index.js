870005c9b81b60e7d61af820eb66bdfcafa7de8a
Maciek Moroz <moroz128@gmail.com>
2021-09-17 00:48:13 +0200

    adjust mobile-horizontal and tablet styles in TeamMember and PortfolioItem components

--- src/components/Portfolio/components/PortfolioItem/index.js
+++ src/components/Portfolio/components/PortfolioItem/index.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const PortfolioItem = ({ name, description }) => {
   return (
-    <div className="column portfolio-item is-4 p-2">
-      <div className="portfolio-item__main p-3">
+    <div className="column portfolio-item is-12-touch is-12-desktop is-12-widescreen is-4-fullhd p-2">
+      <div className="portfolio-item__main p-3 pb-5">
         <p className="title is-size-3 my-4 portfolio-item__name">{name}</p>
-        <p>{description}</p>
+        <p className="portfolio-item__description">{description}</p>
       </div>
     </div>
   );
--- src/components/Team/components/TeamMember/index.js
+++ src/components/Team/components/TeamMember/index.js
@@ -2,12 +2,14 @@ import React from "react";
 
 const TeamMember = ({ name, img, description, offsetY }) => {
   return (
-    <div className="column team-member is-4-tablet is-4-desktop">
-      <p className="subtitle team-member__name">{name}</p>
-      <figure className="image is-square is-fullwidth mb-4 team-member__image">
+    <div className="column team-member is-4">
+      <figure className="image mb-4 team-member__image">
         <img src={img} alt="team member" />
       </figure>
-      <p className="team-member__description">{description}</p>
+      <div className="team-member__text">
+        <p className="team-member__name is-size-5 pb-4">{name}</p>
+        <p className="team-member__description">{description}</p>
+      </div>
     </div>
   );
 };
