3e95c5200adf0f6eb9eaa66c346e67ce4b7f3ccb
Maciek Moroz <moroz128@gmail.com>
2021-09-17 11:54:52 +0200

    additional mobile & tablet styles

--- src/components/Services/components/ServiceTab/index.js
+++ src/components/Services/components/ServiceTab/index.js
@@ -4,15 +4,17 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const ServiceTab = ({ img, title, text, href, buttonText }) => {
   return (
     <div className="column is-4 service-tab">
-      <p className="title service-tab__title">{title}</p>
-      <FontAwesomeIcon icon={img} size="3x" className="mb-5" color="white" />
-      <p className="service-tab__text">{text}</p>
-      <a
-        className="button service-tab__button mb-4 is-primary"
-        href={`/${href}`}
-      >
-        Więcej
-      </a>
+      <div className="container">
+        <p className="title service-tab__title">{title}</p>
+        <FontAwesomeIcon icon={img} size="3x" className="mb-5" color="white" />
+        <p className="service-tab__text">{text}</p>
+        <a
+          className="button service-tab__button mb-4 is-primary"
+          href={`/${href}`}
+        >
+          Więcej
+        </a>
+      </div>
     </div>
   );
 };
